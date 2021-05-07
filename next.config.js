3a399160cb9e46b05e81e1c3a715236eb79e8f7e
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-07 18:45:46 +0200

    chore: config less for antd

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withLess = require('@zeit/next-less');
+const withCSS = require('@zeit/next-css');
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins([
+  [
+    withCSS,
+    withLess({
+      lessLoaderOptions: {
+        modifyVars: {},
+        javascriptEnabled: true,
+      },
+      webpack(config) {
+        return config;
+      },
+    }),
+  ],
+]);
