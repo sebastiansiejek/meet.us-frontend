302fae05bdeab779dbcb92bd5e7ee28f3ea33187
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-09 14:16:39 +0200

    chore: set store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,22 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Provider } from 'react-redux';
+import store from 'store/store';
 
-const queryClient = new QueryClient();
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const queryClient = new QueryClient();
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <QueryClientProvider client={queryClient}>
-    <Hydrate state={pageProps.dehydratedState}>
-      <Component {...pageProps} />
-    </Hydrate>
-    <ReactQueryDevtools />
-  </QueryClientProvider>
-);
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Provider>
+  );
+};
 
 export default MyApp;
