1b3b2e10eea099fee9b2d9286a41f99876db1deb
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-10 21:22:07 +0200

    feat: remove heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import Login from 'components/Login';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hello Next.js 👋</h1>
+  <>
     <Login />
-  </div>
+  </>
 );
 
 export const getStaticProps = async ({ locale }: any) => ({
