1e3b791c2a401b67fd9b1c2553aeec01883d7145
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-11 22:39:14 +0200

    feat: add mobile

--- src/components/JoinUs/JoinUs.tsx
+++ src/components/JoinUs/JoinUs.tsx
@@ -14,12 +14,12 @@ const JoinUs: React.FunctionComponent<JoinUsProps> = ({}) => {
 
   return (
     <Container>
-      <Row gutter={32}>
-        <Col span={12}>
+      <Row gutter={[32, 32]}>
+        <Col lg={12}>
           <Title level={2}>{t('Sign in')}</Title>
           <Login />
         </Col>
-        <Col span={12}>
+        <Col lg={12}>
           <Title level={2}>{t('Sign up')}</Title>
           <Register />
         </Col>
