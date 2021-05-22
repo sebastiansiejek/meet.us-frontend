1fbf683f9b1980a9ca7a22fe06e531a5926b6d8e
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-22 13:57:36 +0200

    feat: style join to us

--- src/pages/join-to-us.tsx
+++ src/pages/join-to-us.tsx
@@ -1,10 +1,15 @@
 import JoinUs from 'src/components/JoinUs/JoinUs';
+import styled from 'styled-components';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+const JoinToUsStyled = styled.div`
+  margin-top: 3rem;
+`;
+
 const JoinToUs = () => (
-  <>
+  <JoinToUsStyled>
     <JoinUs />
-  </>
+  </JoinToUsStyled>
 );
 
 export const getStaticProps = async ({ locale }: any) => ({
