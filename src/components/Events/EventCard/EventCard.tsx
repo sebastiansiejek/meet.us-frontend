d2d1edf3cc53fedc8cbd6ad5aa0e0f0e39b580d9
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-25 22:35:07 +0200

    feat: add tranform animation on hover to event card

--- src/components/Events/EventCard/EventCard.tsx
+++ src/components/Events/EventCard/EventCard.tsx
@@ -3,25 +3,34 @@ import Link from 'next/link';
 import React from 'react';
 import { Event } from 'src/generated/gqlQueries';
 import { getExcerpt } from 'src/utils/excerpt';
+import styled from 'styled-components';
 
 export interface EventCardProps {
   event: Partial<Event>;
 }
 
-const { Meta } = Card;
+const EventCardStyled = styled.a`
+  display: block;
+  transition: transform 0.3s;
+
+  &:hover {
+    transform: translateY(-0.5rem);
+  }
+`;
 
 const EventCard: React.FunctionComponent<EventCardProps> = ({ event }) => {
   const { title, description, id } = event;
+  const { Meta } = Card;
 
   return (
     <Link href={`events/${id}`}>
-      <a>
+      <EventCardStyled>
         <Card title={title}>
           {description && (
             <Meta description={getExcerpt(description, 50)}></Meta>
           )}
         </Card>
-      </a>
+      </EventCardStyled>
     </Link>
   );
 };
