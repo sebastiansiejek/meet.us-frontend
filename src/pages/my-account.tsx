bac92d329f9b431173293f98a4b5f4c793d7cc6a
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-28 15:10:54 +0200

    refactor: move PageHeader to /components

--- src/pages/my-account.tsx
+++ src/pages/my-account.tsx
@@ -1,13 +1,15 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import ActivateUser from 'src/components/ActivateUser';
 import Container from 'src/components/Container';
-import PageHeader from 'src/PageHeader';
+import UserEvents from 'src/components/UserEvents';
+import PageHeader from 'src/components/PageHeader';
 
 const MyAccount = () => {
   return (
     <Container>
       <PageHeader title="My account" />
       <ActivateUser />
+      <UserEvents />
     </Container>
   );
 };
