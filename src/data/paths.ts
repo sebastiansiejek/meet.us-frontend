ee9345b4469d27ded0a677731fb0833e4760d216
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-06-06 11:43:37 +0200

    feat: move navbar links to paths

---
+++ src/data/paths.ts
@@ -0,0 +1,17 @@
+export const paths: Array<{
+  href: string;
+  title: string;
+}> = [
+  {
+    href: '/join-to-us',
+    title: 'Join to us',
+  },
+  {
+    href: '/users',
+    title: 'Użytkownicy',
+  },
+  {
+    href: '/my-account',
+    title: 'My account',
+  },
+];
