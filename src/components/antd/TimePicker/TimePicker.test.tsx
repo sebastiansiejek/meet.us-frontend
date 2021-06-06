0b1400f4b7cc5e21ed7d8cfc659fe428017a61ce
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-06-06 08:49:30 +0200

    feat(draft): add DatePicker antd with dayjs

---
+++ src/components/antd/TimePicker/TimePicker.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import TimePicker, { TimePickerProps } from './TimePicker';
+
+describe('TimePicker', () => {
+    const defaultProps: TimePickerProps = {};
+
+    it('should render', () => {
+        const props = { ...defaultProps };
+        const { asFragment, queryByText } = render(<TimePicker {...props} />);
+
+        expect(asFragment()).toMatchSnapshot();
+        expect(queryByText('TimePicker')).toBeTruthy();
+    });
+});
