26a601e0244fa4503d685e1bf91f308e03485cb7
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-13 23:05:58 +0100

    feat: display future events on homepage if during are empty

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Button, Typography } from 'antd';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import Container from 'src/components/Container';
 import EventCards from 'src/components/Events/EventCards';
 import EventsMap from 'src/components/EventsMap';
@@ -10,13 +11,25 @@ import { Event, useEventsQuery } from 'src/generated/gqlQueries';
 import { routes } from 'src/routes/routes';
 
 const IndexPage = () => {
+  const [state, setState] = useState('DURING');
+
   const { data } = useEventsQuery({
     first: 6,
     orderField: 'startDate',
     orderSort: 'DESC',
     query: '',
-    // state: 'DURING',
+    state,
   });
+
+  useEffect(() => {
+    if (
+      data?.events.page.edges &&
+      data.events.page.edges.length === 0 &&
+      state === 'DURING'
+    ) {
+      setState('FUTURE');
+    }
+  }, [data]);
 
   const { t } = useTranslation();
   const events = data?.events.page.edges as [{ node: Event }];
@@ -28,7 +41,8 @@ const IndexPage = () => {
         <>
           <Container>
             <Typography.Title level={2} className="text-center">
-              {t('During events')}
+              {state === 'DURING' && t('During events')}
+              {state === 'FUTURE' && t('Upcoming events')}
             </Typography.Title>
             <EventCards events={events} />
             <Link href={routes.events.href} passHref>
