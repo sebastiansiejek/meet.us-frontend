80ad28307fd06ab033f89ea82f44f218dc358906
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-13 17:06:43 +0100

    feat: add layout switcher

--- src/components/Events/EventsWithSearch/EventsWithSearch.tsx
+++ src/components/Events/EventsWithSearch/EventsWithSearch.tsx
@@ -8,6 +8,8 @@ import { useEventsQuery } from 'src/generated/gqlQueries';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Container from 'src/components/Container';
 import EventCardSkeleton from '../EventCardSkeleton';
+import { TableOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import EventHorizontal from '../EventHorizontal';
 
 export interface EventsWithSearchProps {
   initSearchQuery: string;
@@ -23,6 +25,7 @@ const EventsWithSearch: React.FunctionComponent<EventsWithSearchProps> = ({
   const [orderField, setOrderField] = useState('startDate');
   const [orderSort, setOrderSort] = useState('DESC');
   const [state, setEventState] = useState('DURING');
+  const [layout, setLayout] = useState<'list' | 'grid'>('grid');
 
   const [endCursor, setEndCursor] = useState('');
   const [isNextPage, setIsNextPage] = useState(true);
@@ -86,32 +89,38 @@ const EventsWithSearch: React.FunctionComponent<EventsWithSearchProps> = ({
       <SearchBar value={`${initSearchQuery}`} />
       {events && (
         <div className="flex flex-col mt-12">
-          <div className="flex">
-            <Select
-              onChange={sortByStateHandler}
-              style={{ width: 200 }}
-              placeholder={t('Select status of events')}
-              className="ml-auto"
-              loading={isLoading}
-              defaultValue={state}
-              value={state}
-            >
-              <Option value="FUTURE">{t('Upcoming')}</Option>
-              <Option value="DURING">{t('During')}</Option>
-              <Option value="PAST">{t('Past')}</Option>
-            </Select>
-            <Select
-              onChange={sortChangeHandler}
-              style={{ width: 200 }}
-              placeholder={t('Select status of events')}
-              className="ml-auto"
-              loading={isLoading}
-              defaultValue={orderSort}
-              value={orderSort}
-            >
-              <Option value="ASC">{t('Ascending by start date')}</Option>
-              <Option value="DESC">{t('Descending by start date')}</Option>
-            </Select>
+          <div className="flex flex-wrap flex-col md:items-center md:flex-row">
+            <div>
+              <Select
+                onChange={sortByStateHandler}
+                style={{ width: 200 }}
+                placeholder={t('Select status of events')}
+                className="ml-auto"
+                loading={isLoading}
+                defaultValue={state}
+                value={state}
+              >
+                <Option value="FUTURE">{t('Upcoming')}</Option>
+                <Option value="DURING">{t('During')}</Option>
+                <Option value="PAST">{t('Past')}</Option>
+              </Select>
+              <Select
+                onChange={sortChangeHandler}
+                style={{ width: 200 }}
+                placeholder={t('Select status of events')}
+                className="ml-auto"
+                loading={isLoading}
+                defaultValue={orderSort}
+                value={orderSort}
+              >
+                <Option value="ASC">{t('Ascending by start date')}</Option>
+                <Option value="DESC">{t('Descending by start date')}</Option>
+              </Select>
+            </div>
+            <div className="md:ml-auto space-x-2">
+              <UnorderedListOutlined onClick={() => setLayout('list')} />
+              <TableOutlined onClick={() => setLayout('grid')} />
+            </div>
           </div>
           <div>
             <InfiniteScroll
@@ -139,7 +148,26 @@ const EventsWithSearch: React.FunctionComponent<EventsWithSearchProps> = ({
               hasMore={isNextPage}
               dataLength={events.length}
             >
-              <EventCards events={events as [{ node: Event }]} />
+              {layout === 'grid' && (
+                <EventCards events={events as [{ node: Event }]} />
+              )}
+              {layout === 'list' && (
+                <Container>
+                  <Row gutter={16}>
+                    {events.map(({ node }) => (
+                      <Col key={node.id} span={24}>
+                        <Space
+                          direction={'vertical'}
+                          size={8}
+                          className="w-full"
+                        >
+                          <EventHorizontal event={node} />
+                        </Space>
+                      </Col>
+                    ))}
+                  </Row>
+                </Container>
+              )}
             </InfiniteScroll>
           </div>
         </div>
