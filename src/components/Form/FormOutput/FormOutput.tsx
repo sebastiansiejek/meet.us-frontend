ac7068f9595f897bbfe4a57842f8bdf6ddf95e41
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-30 20:56:37 +0100

    feat: add form alerts to translations

--- src/components/Form/FormOutput/FormOutput.tsx
+++ src/components/Form/FormOutput/FormOutput.tsx
@@ -1,21 +1,24 @@
 import { Alert } from 'antd';
 import React from 'react';
 import { IApiError } from 'src/types/IApiError';
+import { useTranslation } from 'react-i18next';
 
 export interface FormOutputProps {
   error: IApiError;
 }
 
 const FormOutput: React.FunctionComponent<FormOutputProps> = ({ error }) => {
+  const { t } = useTranslation();
+
   return (
     <>
       {error?.data?.message &&
         Array.isArray(error.data.message) &&
         error.data.message.map((error) => (
-          <Alert message={error} type="error" key={error} />
+          <Alert message={t(error)} type="error" key={error} />
         ))}
       {error?.data?.message && !Array.isArray(error.data.message) && (
-        <Alert message={error.data.message} type="error" />
+        <Alert message={t(error.data.message)} type="error" />
       )}
     </>
   );
