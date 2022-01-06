a5df64f0ff240ef5b554ff44e29f9d61ccaa755f
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-01-06 17:55:18 +0100

    feat: redirect logged user from /join-to-us to /my-account page

--- src/pages/join-to-us.tsx
+++ src/pages/join-to-us.tsx
@@ -1,6 +1,9 @@
 import JoinUs from 'src/components/JoinUs/JoinUs';
 import styled from 'styled-components';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetServerSidePropsContext } from 'next';
+import { getSession } from 'next-auth/react';
+import { routes } from 'src/routes/routes';
 
 const JoinToUsStyled = styled.div`
   margin-top: 3rem;
@@ -12,10 +15,24 @@ const JoinToUs = () => (
   </JoinToUsStyled>
 );
 
-export const getStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common'])),
-  },
-});
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const session = await getSession(ctx);
+  const { locale }: any = ctx;
+
+  if (session?.accessToken) {
+    return {
+      redirect: {
+        destination: routes.myAccount.href,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+};
 
 export default JoinToUs;
