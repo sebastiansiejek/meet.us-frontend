dafb461d50d55e2b1dbd721bd85168481da4392a
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-01-06 10:45:56 +0100

    feat: use request in useFetchData

--- src/utils/useFetchData.ts
+++ src/utils/useFetchData.ts
@@ -1,33 +1,8 @@
-import { getCookieToken } from 'src/services/AuthService';
+import { request } from './request';
 
 export const useFetchData = <TData, TVariables>(
   query: string,
+  variables?: TVariables,
 ): (() => Promise<TData>) => {
-  return async (variables?: TVariables) => {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${getCookieToken()}`,
-        'Accept-Language':
-          document.querySelector('html')?.getAttribute('lang') || 'en',
-      },
-      body: JSON.stringify({
-        query,
-        variables,
-      }),
-    });
-
-    const json = await res.json();
-
-    if (json.errors) {
-      const error: Error = new Error(json.errors[0].message);
-      throw {
-        error,
-        ...{ data: json.errors[0].extensions.exception.response },
-      };
-    }
-
-    return json.data;
-  };
+  return async () => request(query, variables);
 };
