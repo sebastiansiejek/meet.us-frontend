ee72972b055bc532b5d3bcfa21a773e56a1f17b3
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-01-06 12:31:02 +0100

    feat: add session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,8 +12,12 @@ import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { appWithTranslation } from 'next-i18next';
+import { SessionProvider } from 'next-auth/react';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp: React.FC<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   NProgress.configure({ showSpinner: true });
   Router.events.on('routeChangeStart', () => NProgress.start());
   Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -22,16 +26,18 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const queryClient = new QueryClient();
 
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <GlobalStyles />
-          <Navbar />
-          <Main Component={Component} pageProps={pageProps} />
-        </Hydrate>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <GlobalStyles />
+            <Navbar />
+            <Main Component={Component} pageProps={pageProps} />
+          </Hydrate>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </Provider>
+    </SessionProvider>
   );
 };
 
