400d3931d0859485e38421a2fa8a6a945732dc8a
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-01-07 14:54:32 +0100

    feat: change myAccount static route with from array

--- src/components/ActivateUser/ActivateUser.tsx
+++ src/components/ActivateUser/ActivateUser.tsx
@@ -6,30 +6,25 @@ import { Alert, Spin } from 'antd';
 import FormOutput from '../Form/FormOutput';
 import { IApiError } from 'src/types/IApiError';
 import { useTranslation } from 'react-i18next';
+import { routes } from 'src/routes/routes';
 
 export interface ActivateUserProps {}
 
 const ActivateUser: React.FunctionComponent<ActivateUserProps> = ({}) => {
   const { t } = useTranslation();
-
   const router = useRouter();
-  const {
-    mutateAsync,
-    isLoading,
-    error,
-    data,
-    isSuccess,
-  } = useActivateUserMutation();
-
+  const { mutateAsync, isLoading, error, data, isSuccess } =
+    useActivateUserMutation();
   const { token } = router.query;
 
   useEffect(() => {
     if (token) {
       mutateAsync({ token: token.toString().split(' ').join('+') }).then(() => {
-        router.replace('/my-account', undefined, { shallow: true });
+        router.replace(routes.joinToUs.href, undefined, { shallow: true });
       });
     }
   }, [token]);
+
   return (
     <>
       {token && (
