c5165fff54e4fbc0d20444228f702cfb64b501b5
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-01-09 14:05:47 +0100

    feat: add IEventParticipant

--- src/types/IEvent.ts
+++ src/types/IEvent.ts
@@ -1,1 +1,8 @@
 export const getMapEventTypes: Array<string> = ['Sport', 'Party', 'Chatting'];
+
+/**
+ * 0 - Nothing
+ * 1 - Following
+ * 2 - Joined
+ */
+export type IEventParticipant = 0 | 1 | 2;
