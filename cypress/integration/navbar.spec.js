8d0bedf1ddddc3eb6a054ea9f59bc003eb109bbc
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-02-19 17:19:26 +0100

    test: fix logo redirect

--- cypress/integration/navbar.spec.js
+++ cypress/integration/navbar.spec.js
@@ -4,6 +4,7 @@ describe('Navbar', () => {
   it('click on site logo in navbar should redirect to homepage', () => {
     const homepageUrl = 'http://localhost:3001/';
 
+    cy.viewport(1920, 1080);
     cy.visit(homepageUrl);
     cy.get('[data-cy="navbar-logo"]').click();
     cy.url().should('include', homepageUrl);
